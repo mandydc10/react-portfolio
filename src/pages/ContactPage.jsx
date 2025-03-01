0aff04a59a51d09fc4cd6153bf636618dea758f4
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2025-03-01 17:28:54 +0800

    fix: bug with top of page hidden by header

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,14 +1,14 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import ContactForm from "../components/forms/ContactForm.jsx";
 import AboutSection from "../components/forms/ContactForm.jsx";
 import "./ContactPage.css";
 // import { pagesText } from "../projectData.js"
 
 function ContactPage() {
-    // useEffect(() => {
-    //     // Scroll to the top when the component mounts
-    //     window.scrollTo(0, 0);
-    // }, []);
+    useEffect(() => {
+        // Scroll to the top when the component mounts
+        window.scrollTo(0, 0);
+    }, []);
 
     return (
         <div className="contact-page">
