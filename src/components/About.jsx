91b1ff84cbe188874511bdfada91fe0cb6826ee4
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2025-03-01 11:48:15 +0800

    Update About.jsx
    Revised final sentence on personal hobbies.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
                 </p>
                 <p>What drives me most is the opportunity to solve meaningful challenges through technology while continuously learning and growing. I’m a strong believer in fostering collaboration and maintaining a respectful, inclusive environment, whether working in a team or engaging with users.
                 </p>
-                <p>When I’m not coding, you might find me mentoring others in the developer community, exploring creative hobbies, or brainstorming ways to make life easier through automation. My portfolio is a reflection of my journey so far and the skills I’m excited to keep building upon.
+                <p>When I’m not coding, you might find me mentoring others in the developer community, exploring creative hobbies like dressmaking, leather crafting, training handstands or gymnastic rings, brushing up on the latest health and nutrition research, checking out my local dog shelter, or brainstorming ways to try and improve life for others through technology. My portfolio is a reflection of my journey so far and the skills I’m excited to keep building upon.
                 </p>
                 <p>Feel free to explore my projects, and don’t hesitate to reach out—I’d love to connect!
                 </p>
