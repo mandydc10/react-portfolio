2d81508b112d49cc862bdc71936b4b2071e01c05
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2025-03-01 17:50:21 +0800

    fix: added honeypot input line as comment in contact form component and added non-breaking space in form input name for easier readability with the email submission

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -4,10 +4,11 @@ function ContactForm() {
     return (
         <>
             <form action="https://formsubmit.co/9cbd80d6cabd97a9392bf66804ed2353" method="POST">
+                {/* honeypot -> <input type="text" name="_honey" style="display: none;"> Disable Captcha -> <input type="hidden" name="_captcha" value="false"> */}
                 <label htmlFor="firstName">First Name</label>
                 <input type="text" name="name" id="firstName" placeholder="First name" required />
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="Last Name" id="lastName" placeholder="Last name" required />
+                <input type="text" name="last&nbsp;name" id="lastName" placeholder="Last name" required />
                 <label htmlFor="email">Contact email</label>
                 <input type="text" name="email" id="email" placeholder="Type your email address" required />
                 <label htmlFor="message">Message:</label>
